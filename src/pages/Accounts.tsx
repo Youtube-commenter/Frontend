f47d42c93ce7f0ef2fd928f6420149688d206d75
Oussama Elkamell <oussamaaelkamel@gmail.com>
2025-03-08 17:48:21 +0100

    add client ids

--- src/pages/Accounts.tsx
+++ src/pages/Accounts.tsx
@@ -143,7 +143,7 @@ const Accounts = () => {
           <div className="flex items-center">
             <Avatar className="mr-2 h-8 w-8">
               <AvatarImage src={account.thumbnailUrl} alt={account.channelTitle || account.email} />
-              <AvatarFallback>{account.email.toUpperCase()}</AvatarFallback>
+              <AvatarFallback>o</AvatarFallback>
             </Avatar>
             <div>
               <CardTitle className="text-base font-medium">
